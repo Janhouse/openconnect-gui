fc72eb405651c64501e97aa85cf0b70ca692cd34
Benjamin Byholm <bbyholm@abo.fi>
2020-04-07 13:34:16 +0300

    openconnect: bump to 8.08

--- CMake/Includes/ProjectExternals.cmake
+++ CMake/Includes/ProjectExternals.cmake
@@ -1,5 +1,5 @@
-set(openconnect-TAG "v8.07" CACHE STRING "Please specify openconnect version")
-set(openconnect-TAG_CHOICES "v8.07" "master")
+set(openconnect-TAG "v8.08" CACHE STRING "Please specify openconnect version")
+set(openconnect-TAG_CHOICES "v8.08" "master")
 set_property(CACHE openconnect-TAG PROPERTY STRINGS ${openconnect-TAG_CHOICES})
 if(NOT openconnect-TAG IN_LIST openconnect-TAG_CHOICES)
   message(FATAL_ERROR "Specify 'openconnect-TAG'. Must be one of ${openconnect-TAG_CHOICES}")
